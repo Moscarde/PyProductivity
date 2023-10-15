7e197ebcef49b96c72e2b7adb16c26ea6cf87236
Gabriel Moscarde <78855746+Moscarde@users.noreply.github.com>
2023-10-15 20:46:16 -0300

    refactoring

--- main.py
+++ main.py
@@ -1,234 +1,140 @@
-# pylint: disable=import-error
-from icecream import ic
-import pygetwindow as gw
-from datetime import datetime, timedelta
 import os
 import time
+from datetime import datetime, timedelta
+
 import pyautogui
+import pygetwindow as gw
+from icecream import ic
 
-# ic.disable()
-
-sleep_loop = 9
-inactivity_timer_in_seconds = 0
+tick_seconds = 9  # max 9
+inactive_time = 0
+max_inactive_time = 600
 previous_mouse_pos = [0, 0]
-inactivity = 600
-line_separator = '  <~~>  '
-data_separator = '<~~~~~~~~~~~~~~~~~~~~~~~~ Usage time - Specific ~~~~~~~~~~~~~~~~~~~~~~~~>'
-base_doc = '''<~~~~~~~~~~~~~~~~~~~~~~~~~~~ Usage time - Sum ~~~~~~~~~~~~~~~~~~~~~~~~~~>
+
+line_separator = "  <~~>  "
+data_separator = (
+    "<~~~~~~~~~~~~~~~~~~~~~~~~ Usage time - Specific ~~~~~~~~~~~~~~~~~~~~~~~~>"
+)
+base_data = """<~~~~~~~~~~~~~~~~~~~~~~~~~~~ Usage time - Sum ~~~~~~~~~~~~~~~~~~~~~~~~~~>
 
 <~~~~~~~~~~~~~~~~~~~~~~~~ Usage time - Specific ~~~~~~~~~~~~~~~~~~~~~~~~>
-'''
+"""
 
 
-def check_ausencia():
-    global previous_mouse_pos, inactivity_timer_in_seconds
+def is_active():
+    global previous_mouse_pos, inactive_time
     mouse_pos = list(pyautogui.position())
 
     if mouse_pos == previous_mouse_pos:
-        if inactivity_timer_in_seconds > inactivity:
-            print('inative')
+        if inactive_time > max_inactive_time:
+            print("inative")
             return False
         else:
-            inactivity_timer_in_seconds += sleep_loop
+            inactive_time += tick_seconds
             return True
     else:
         previous_mouse_pos = mouse_pos
-        inactivity_timer_in_seconds = 0
+        inactive_time = 0
         return True
 
 
-def read_today_data(date):
-    if not os.path.exists(f'{date}.txt'):
-        file = open(f'{date}.txt', 'w')
-        file.write(base_doc)
-        today_data = base_doc.split('\n')
+def read_data(date):
+    if not os.path.exists(f"{date}.txt"):
+        file = open(f"{date}.txt", "w")
+        file.write(base_data)
+        data = base_data.split("\n")
         file.close()
-        return today_data
+        return data
 
-    file = open(f'{date}.txt', 'r', encoding='UTF-8')
-    today_data = file.read().split('\n')
-    file.close()
-    return today_data
+    else:
+        file = open(f"{date}.txt", "r", encoding="UTF-8")
+        data = file.read().split("\n")
+        file.close()
+        return data
 
 
-def write(today, data):
-    file = open(f'{today}.txt', 'w', encoding='UTF-8')
+def write_data(date, data):
+    file = open(f"{date}.txt", "w", encoding="UTF-8")
     file.write(data)
     file.close()
 
 
-def add_time(window, data, full_line):
-    window_line_index = data.index(full_line)
+def add_time(line):
+    timestamp, window_title = line.split(line_separator)
 
-    time = full_line.split(line_separator)[0]
+    timestamp = datetime.strptime(timestamp, "%H:%M:%S").time()
+    converted_timestamp = datetime.combine(datetime.min, timestamp)
 
-    time = datetime.strptime(time, '%H:%M:%S').time()
-    new_time = (datetime.combine(datetime.min, time) +
-                timedelta(seconds=sleep_loop)).time()
-    new_time = new_time.strftime('%H:%M:%S')
-    data[window_line_index] = f"{new_time}  <~~>  {window}"
-    return data
-
-
-def short_add_time(line):
-    time, term = line.split(line_separator)
-    # ic(time)
-    # ic(term)
-    time = datetime.strptime(time, '%H:%M:%S').time()
-    new_time = (datetime.combine(datetime.min, time) +
-                timedelta(seconds=sleep_loop)).time()
-    new_time = new_time.strftime('%H:%M:%S')
-    line = f"{new_time}{line_separator}{term}"
+    new_timestamp = (converted_timestamp + timedelta(seconds=tick_seconds)).time()
+    new_timestamp = new_timestamp.strftime("%H:%M:%S")
+    line = f"{new_timestamp}{line_separator}{window_title}"
     return line
 
 
-def get_line(term, data):
+def get_line(window_title, data):
     try:
-        line = list(filter(lambda line: term in line, data))[0]
-        return line
+        line = list(filter(lambda line: window_title in line, data))[0]
+        index = data.index(line)
+        return line, index
 
     except:
-        return None
+        return None, None
 
 
-def get_line_index(line, data):
-    try:
-        line = data.index(line)
-        return line
-
-    except:
-        return None
-
-
-def get_processed_data(term, data):
-    # term = YouTube — Mozilla Firefox // full
-    # data = stats or processed
-
-    # get line
-    # get time
-    # sum time
-    # replace line
-    # return data
-
-    # ic(term, data)
-
-    old_line = get_line(term, data)  # v
-    old_line_index = get_line_index(old_line, data)  # v
-    # ic(old_line, old_line_index)
-    # ic(term, data)
+def get_processed_data(window_title, data):
+    old_line, old_line_index = get_line(window_title, data)
 
     if old_line == None:
         # Insere uma nova linha caso nao encontre
-        new_line = f"00:00:0{sleep_loop}  <~~>  {term}"
+        new_line = f"00:00:0{tick_seconds}  <~~>  {window_title}"
         data.insert(-1, new_line)
     else:
-        #substitui uma linha 
-        new_line = short_add_time(old_line)  # v
+        # Substitui uma linha
+        new_line = add_time(old_line)
         data[old_line_index] = new_line
 
     return data
 
 
-def process_log_stats(window, full_data):
-    # in
-    # main.py - PyWindows - Visual Studio Code
-    # # ['<~~~~~~~~~~~~stats~~~~~~~~~~~~~~>',
-    #             '00:07:52  <~~>  Visual Studio Code',
-    #             '00:00:20  <~~>  WakaTime — Mozilla Firefox',
-    #             '00:00:35  <~~>  YouTube — Mozilla Firefox',
-    #             '00:00:05  <~~>  Hearts / Wires',
-    #             '<~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>',
-    #             '00:00:14  <~~>  main.py - PyWindows - Visual Studio Code',
-    #             '00:01:33  <~~>  2023-10-15.txt - PyWindows - Visual Studio Code']
-
-    #ic(full_data) # entrada
-
-    # spliting datas #v
+def process_report(window, full_data):
+    # spliting datas
     separator_index = full_data.index(data_separator)
     data_stats = full_data[:separator_index]
     data_logs = full_data[separator_index:]
-    #ic(data_stats)
-    #ic(data_logs)
 
-    # processing stats data
-    window_app = window.split(' - ')[-1]
-
-    data_stats = get_processed_data(window_app, data_stats)
+    # processing data
     data_logs = get_processed_data(window, data_logs)
+    window_app = window.split(" - ")[-1]
+    data_stats = get_processed_data(window_app, data_stats)
 
     full_data = data_stats + data_logs
 
-    #ic(full_data)  # saida
     return full_data
 
 
 def report(window):
-    today = datetime.now().strftime("%Y-%m-%d")
-    data = read_today_data(today)
+    todays_date = datetime.now().strftime("%Y-%m-%d")
+    data = read_data(todays_date)
+    
+    # console log
+    ic("report", window)
+    data = process_report(window, data)
 
-    # testes
-    ic('report', window)
-
-    data = process_log_stats(window, data)
-
-    data_string = '\n'.join(str(line) for line in data)
-    write(today, data_string)
-
-    # ler data - verificar se tem data da janela - somar ou adicionar uma do zero
+    data_string = "\n".join(str(line) for line in data)
+    write_data(todays_date, data_string)
 
 
-def monitore_windows():
+def main():
     repeat = True
     while repeat:
 
-        if check_ausencia():
+        if is_active():
             active_window = gw.getActiveWindowTitle()
             if active_window != None:
                 report(active_window)
-            else:
-                ic('None Window')
-        time.sleep(sleep_loop)
+
+        time.sleep(tick_seconds)
 
 
 if __name__ == "__main__":
-    monitore_windows()
-    # sum_log_stats('00:01:31  <~~>  2023-10-15.txt - PyWindows - Visual Studio Code', read_today_data(datetime.now().strftime("%Y-%m-%d")))
-    # process_log_stats('2023-10-15.txt - xxxxxxx', read_today_data(datetime.now().strftime("%Y-%m-%d")))
-    
-
-
-
-# CC ============================================
-def read_today(today):
-    if not os.path.exists(f'{today}.txt'):
-        file = open(f'{today}.txt', 'w')
-        file.close()
-        return ['TOTAL_TIME:', '0:00:00', 'LOG:']
-
-    file = open(f'{today}.txt', 'r')
-    study_duration = file.read().split('\n')
-    file.close()
-    return study_duration
-
-
-def calc_elapsed_time(stored, studied):
-    elapsed_time = datetime.strptime(stored, "%H:%M:%S") + studied
-    elapsed_time = elapsed_time.strftime("%H:%M:%S")
-    return elapsed_time
-
-
-def write_today(today, study_duration):
-    today_data = read_today(today)
-
-    today_data[1] = calc_elapsed_time(today_data[1], study_duration)
-    today_data.append(
-        f"Date {datetime.now().strftime('%m/%d/%Y, %H:%M:%S')} - Time Spent {round_delta(study_duration)}")
-
-    today_data_string = '\n'.join(str(line) for line in today_data)
-
-    file = open(f'{today}.txt', 'w')
-    file.write(today_data_string)
-    file.close()
-
-
-def round_delta(x): return str(x).split('.')[0]
+    main()

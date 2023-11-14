dcb72a0008d06edcde9ac7b352338089b1ccdd09
Gabriel Moscarde <78855746+Moscarde@users.noreply.github.com>
2023-11-14 12:15:11 -0300

    create DataHandler

--- pyproductivity.OO.py
+++ pyproductivity.OO.py
@@ -4,7 +4,8 @@ import shutil
 import subprocess
 import tkinter as tk
 import webbrowser
-from tkinter import filedialog, messagebox, ttk
+from tkinter import messagebox, ttk
+from datetime import timedelta
 
 import matplotlib.cbook as cbook
 import matplotlib.pyplot as plt
@@ -13,15 +14,54 @@ import numpy as np
 import win32con
 import win32gui
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from PIL import Image, ImageTk
-
-from modules.report import get_df_report
-
+import pandas as pd
 
 class Constants:
     BG_COLOR = "#E6E6FA"
     BUTTON_COLOR = "#CBC3E3"
     BUTTON_ACTIVE_COLOR = "#603FFE"
+
+class DataHandler:
+    @staticmethod
+    def usage_time_acum(path):
+        data = pd.read_csv(path, parse_dates=["timestamp"])
+
+        data["name"] = data["app_name"].map(lambda x: x.split(" — ")[-1].split(" - ")[-1])
+        data.columns = ["timestamp", "window_title", "minutes_away", "app_name"]
+        data = data.sort_values(["app_name", "timestamp"])
+
+        data["daily_usage"] = data.apply(DataHandler.timestamps_acum, axis=1)
+
+        data = (
+            data.groupby("app_name")["daily_usage"]
+            .max()
+            .sort_values(ascending=False)
+        )
+        data = data.to_frame().reset_index()
+        data['daily_usage_seconds'] = data['daily_usage'].apply(lambda x: pd.Timedelta(x).total_seconds()).astype(int)
+        return data
+
+
+    last_timestamp = timedelta(seconds=0)
+    last_app_name = "x"
+    total_delta = timedelta(seconds=0)
+
+    @staticmethod
+    def timestamps_acum(row):
+        if (str(DataHandler.last_timestamp) == "0:00:00") or (row["app_name"] != DataHandler.last_app_name):
+            DataHandler.last_timestamp = row["timestamp"]
+            DataHandler.last_app_name = row["app_name"]
+            DataHandler.total_delta = timedelta(seconds=0)
+            return timedelta(seconds=0)
+
+        elif (row["timestamp"] - DataHandler.last_timestamp) < timedelta(seconds=40):
+            delta = row["timestamp"] - DataHandler.last_timestamp
+            DataHandler.total_delta += delta
+            return DataHandler.total_delta
+
+        else:
+            DataHandler.last_timestamp = row["timestamp"]
+            return DataHandler.total_delta
 
 
 class FileHandler:
@@ -301,7 +341,7 @@ class AnalysisInterface:
             filename = event.widget.get()
 
         csv_path = os.path.join(os.getcwd(), "logs", filename)
-        df = get_df_report(csv_path)
+        df = DataHandler.usage_time_acum(csv_path)
 
         options = {
             "qtd_apps": 5,
